90ba6b2e5975c2c57c016867045f46e02e07dda1
Vien Van <vienvonvan@gmail.com>
2016-09-11 01:42:08 -0700

    show dynamic data on clicked topic

--- public/app.js
+++ public/app.js
@@ -3,14 +3,14 @@ console.log("im not crazy");
 var app = angular.module("ngWittily", ["firebase"]);
 
 //Creating controller
-app.controller("ScreenCtrl", ["$scope", "$firebaseObject", function($scope,  $firebaseObject)
+app.controller("ScreenCtrl", ["$scope", "$firebaseObject", function($scope,  $firebaseArray)
 {
-    var ref = new Firebase("https://letsdebate-acb88.firebaseio.com/");
+    var ref = new Firebase("https://letsdebate-acb88.firebaseio.com/topics");
     // download the data into a local object
-    var syncObject = $firebaseObject(ref);
+    var syncArray = $firebaseArray(ref);
     // synchronize the object with a three-way data binding
     // click on `index.html` above to see it used in the DOM!
-    syncObject.$bindTo($scope, "data");
+    syncArray.$bindTo($scope, "data");
 
 
     $scope.affirmScore = 0;
@@ -22,6 +22,10 @@ app.controller("ScreenCtrl", ["$scope", "$firebaseObject", function($scope,  $fi
 
     $scope.affirmAddOne = function() {
       $scope.affirmScore++;
+    }
+
+    $scope.showDetails = function(topicId) {
+      $scope.queriedObject = $scope.data[topicId];
     }
 
     // $scope.arguments = $scope.data.topics.arguments
